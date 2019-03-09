d2618724023bd53dfbe535ec75ca8870ae29daff
xyzhang <always.zhang@gmail.com>
2019-03-09 23:00:50 +0800

    add new `spv` namespace , `spv` will be served as the base structure for all different SPV in structure product.

--- src/clj/clojucture/spv.clj
+++ src/clj/clojucture/spv.clj
@@ -5,7 +5,8 @@
             [java-time :as jt]
             [clojucture.util :as u]
             [clojure.java.io :as io]
-            [clojure.core.match :as m])
+            [clojure.core.match :as m]
+            [clojure.pprint :as pp])
   (:import [tech.tablesaw.api ColumnType Table]
            [tech.tablesaw.columns AbstractColumn Column]
            [java.time LocalDate]
@@ -24,7 +25,7 @@
         :M (u/gen-dates-range first-collect-date (jt/months 1) stated-maturity)
         :BM (u/gen-dates-range first-collect-date (jt/months 2) stated-maturity)
         :Y (u/gen-dates-range first-collect-date (jt/years 1) stated-maturity))
-      (cons closing-date)
+      (conj closing-date)
       (u/gen-dates-interval )
       )
   )
@@ -35,12 +36,24 @@
          first-payment-date :first-payment-date
          payment-interval   :payment-interval
          stated-maturity    :stated-maturity} (:dates deal-info)]
+    (->
       (case payment-interval
         :Q (u/gen-dates-range first-payment-date (jt/months 3) stated-maturity)
         :M (u/gen-dates-range first-payment-date (jt/months 1) stated-maturity)
         :BM (u/gen-dates-range first-payment-date (jt/months 2) stated-maturity)
         :Y (u/gen-dates-range first-payment-date (jt/years 1) stated-maturity))
-      ))
+      (vec)
+      (conj stated-maturity)(seq))
+    )
+  )
+
+(defn gen-pool-cf-split-dates [ deal-info ]
+  (let [ closing-date (get-in deal-info [:dates :closing-date])
+        dd (get-in deal-info [:dates :delay-days])
+        split-dates (map #(jt/adjust % jt/minus (jt/days dd))  (deal-info :b-payment-dates)) ]
+    (->>
+      (cons closing-date split-dates)
+      (partition 2 1)) ) )
 
 (defn init-bondflow [ deal-info bonds ]
   (let [ payment-dates (gen-bond-payment-date deal-info)
@@ -51,28 +64,36 @@
         ]
     (.addColumns dates-column tb) ) )
 
-(defrecord deal [ info status pool waterfall bond ])
-
+;; simple deal structure without any localization
+;; (defrecord deal [ info status pool waterfall bond ])
+(defn b-find-cutoff [ d ^LocalDate payment-date ]
+  (let [ dd (get-in d [:info :delay-days])]
+    
+    
+    )
+  
+  )
 
 (defn init-deal [ d ]
   (when-let [deal d]
-    (let [ i (:info deal)
-          update-date (get-in d [:status :update-date])]
-      (println i)
-      (-> d
+    (let [ update-date (get-in deal [:status :update-date])]
+;      (println (gen-pool-cf-split-dates i))
+      (as-> deal deal-setup
           ;; deal original info
-          (assoc-in [:info :p-collection-intervals] (gen-pool-collect-interval i))
-          (assoc-in [:info :b-payment-dates] (gen-bond-payment-date i))
+          (assoc-in deal-setup [:info :p-collection-intervals] (gen-pool-collect-interval (deal-setup :info)))
+          (assoc-in deal-setup [:info :b-payment-dates] (gen-bond-payment-date (deal-setup :info)))
+          (assoc-in deal-setup [:info :b-aggregate-intervals] (gen-pool-cf-split-dates (deal-setup :info)))
 
 
           ;; update info
-          (assoc-in [:status :b-rest-payment-dates]
-                    (filter #(jt/after? % update-date) (get-in d [:info :b-payment-dates])))
-          (assoc-in [:status :p-rest-collection-intervals]
-                    (filter #(jt/after? % update-date) (get-in d [:info :p-collection-intervals]))
-                    )
+          (assoc-in deal-setup [:status :b-rest-payment-dates]
+                    (filter #(jt/after? % update-date) (get-in deal-setup [:info :b-payment-dates])))
+          (assoc-in deal-setup [:status :p-rest-collection-intervals]
+                    (filter #(jt/after? (first %) update-date) (get-in deal-setup [:info :p-collection-intervals]))
+                   )
 
-          )
+        )
+
       )
     )
   )
@@ -82,7 +103,7 @@
   (->
     (m/match m
     {:info i :status s :pool p :waterfall w :bond b}
-      (deal. i s p w b )
+      m
     :else nil
     )
     (init-deal))
@@ -102,12 +123,12 @@
 )
 
 (defn run-bonds [ d ]
-  (let [ {update-date :update-date} (:status d)
-        bond-rest-payment-dates (drop-while #(jt/before? % update-date) (get-in (:info d) :b-payment-dates))
-
-        ]
-
-
+  (let [ bond-rest-payment-dates (get-in d [:info :b-rest-payment-dates ]) ]
+    (doseq [ pd bond-rest-payment-dates]
+      
+      
+      )
+    nil
     )
 
   )
--- test/clojucture/spv_test.clj
+++ test/clojucture/spv_test.clj
@@ -6,7 +6,7 @@
 
 
 
-;test build deal
+;test build deal ,test_001
 
 (deftest t-build-deal
   (let [ m
@@ -17,8 +17,9 @@
             :collect-interval :M
             :stated-maturity (jt/local-date 2025 1 1)
             :settle-date (jt/local-date 2017 4 1)
-            :first-payment-date (jt/local-date 2017 5 1)
-            :payment-interval :Q}
+            :first-payment-date (jt/local-date 2017 5 26)
+            :payment-interval :Q
+            :delay-days 26}
             }
          :status {:update-date (jt/local-date 2019 3 6)}
          :pool  nil
@@ -30,17 +31,33 @@
     ; test on original info
     (let [ di (:info tdeal)
           p-intervals (:p-collection-intervals di)
-          b-payment-dates (:b-payment-dates di)]
+          b-agg-intervals (:b-aggregate-intervals di)
+          b-payment-dates (:b-payment-dates di)
 
+          ]
       (is (= (ffirst p-intervals) (jt/local-date 2017 1 1)))
-      (is (= (second (last p-intervals))  (jt/local-date 2025 1 1)))
+      (is (= (second (last p-intervals))  (jt/local-date 2024 11 30)))
 
-      (is (= (first b-payment-dates) (jt/local-date 2017 5 1)))
+
+      (is (= (ffirst b-agg-intervals) (jt/local-date 2017 1 1)))
+      (is (= (second (first b-agg-intervals)) (jt/local-date 2017 4 30)))
+
+      (is (= (first b-payment-dates) (jt/local-date 2017 5 26)))
       (is (= (last b-payment-dates) (jt/local-date 2025 1 1)))
 
       )
 
     ; test on update info
+    (let [ ds (:status tdeal)
+          p-rest-coll-intervals (:p-rest-collection-intervals ds)
+          b-rest-payment-dates (:b-rest-payment-dates ds)
+          ]
+      (is (= (ffirst p-rest-coll-intervals) (jt/local-date 2019 4 1 )))
+      (is (= (first (last p-rest-coll-intervals))  (jt/local-date 2024 11 1)  ))
 
+      (is (= (first b-rest-payment-dates) (jt/local-date 2019 5 26)  ))
+      (is (= (last b-rest-payment-dates) (jt/local-date 2025 1 1) ))
+
+      )
     )
   )
