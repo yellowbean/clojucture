b99d461db791f963ee61abffccd933484e697e45
xyzhang <always.zhang@gmail.com>
2019-05-17 00:39:22 +0800

    add setup for graalvm

--- project.clj
+++ project.clj
@@ -14,16 +14,31 @@
     [org.clojure/data.csv "0.1.4"]
     [org.clojure/data.json "0.2.6"]
     [org.clojure/data.zip "0.1.2"]
-    [org.clojure/core.match "0.3.0-alpha5"]
+    [org.clojure/core.match "0.3.0"]
     [dk.ative/docjure "1.12.0"]
    ]
   :plugins [
             [lein-virgil "0.1.9"]
             ]
-
+  :main clojucture.core
   :mirrors {"central" {:name "aliyun maven"
                        :url "https://maven.aliyun.com/nexus/content/groups/public/"}}
+  ;:user {:plugins [ [nightlight/lein-nightlight "RELEASE"]]}
   :source-paths ["src/clj"]
   :java-source-paths [ "src/java" ]
-  :profiles {:uberjar {:aot :all}}
-  )
+  :profiles {
+   :uberjar {:aot :all}
+   :dev {
+    :plugins [
+      [ lein-shell "0.5.0"]
+    ]
+   }
+  }
+  :aliases
+  {"native"
+   ["shell"
+    "native-image" "--report-unsupported-elements-at-runtime"
+    "--initialize-at-build-time"
+    "-jar" "./target/${:uberjar-name:-${:name}-${:version}-standalone.jar}"
+    "-H:Name=./target/${:name}"]}
+)
--- src/clj/clojucture/core.clj
+++ src/clj/clojucture/core.clj
@@ -1,13 +1,13 @@
 (ns clojucture.core
   (:import
-    [tech.tablesaw.api Table DoubleColumn DateColumn]
-    [tech.tablesaw.columns AbstractColumn]
-    [org.apache.commons.math3.complex Complex])
-  (:import
-    [tech.tablesaw.api Table DoubleColumn DateColumn StringColumn BooleanColumn]
     [java.time LocalDate])
+  (:gen-class)
   )
 
 (defrecord stmt [ ^LocalDate date from to ^Double amount info ]
 
   )
+
+(defn -main [ & args]
+  (println "reading from input"))
+
