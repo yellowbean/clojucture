7d8e79aff3c7accffbc9401bc895b608bf71b90f
xyzhang <always.zhang@gmail.com>
2019-06-18 23:11:52 +0800

    add 'balance' field for cashflow test case

--- test/clojucture/cashflow_test.clj
+++ test/clojucture/cashflow_test.clj
@@ -2,18 +2,20 @@
   (:require [clojure.test :refer :all]
             [clojucture.asset :as asset]
             [java-time :as jt]
-            [clojucture.util :as u]
-            )
-)
+            [clojucture.util :as u]))
+            
+
 (def sample-cf
   (let [ df (u/gen-dates-ary (jt/local-date 2018 1 1) (jt/months 1) 12)
-        bf (double-array 12 200)
-        if (double-array [0 10 20 30 40 50 60 70 80 90 100 110]) ]
-  (u/gen-cashflow "sample1"
-    [ [:dates df ]
-      [:principal bf]
-      [:interest if]
-     ] )
-))
+        balf (double-array (range 2200 -1 -200))
+        bf (aset-double (double-array 12 200) 0 0)
+        if (double-array [0 10 20 30 40 50 60 70 80 90 100 110])]
+   (u/gen-cashflow "sample1"
+     [ [:dates df]
+      [:balance balf
+       [:principal bf]
+       [:interest if]]])))
+      
+
 
 
