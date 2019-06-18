4f179dfcd6bb5f2793d67d057d5e526277764761
xyzhang <always.zhang@gmail.com>
2019-06-18 23:10:38 +0800

    assumption

--- src/clj/clojucture/assumption.clj
+++ src/clj/clojucture/assumption.clj
@@ -4,78 +4,80 @@
             [clojure.core.match :as m]
             [clojucture.util :as u])
   (:import [java.time LocalDate]
-           clojucture.DoubleFlow
-           (clojucture RateAssumption) ) )
+           [clojucture DoubleFlow]
+           [clojucture RateAssumption]))
 
 
 
-(defn pick-rate-by-date [ ^LocalDate d index-curve ]
-  (let [shifted-curve (for [ [ d r ] index-curve] [ (jt/plus d (jt/days -1)) r ] )]
-    (second (last (filter #(jt/after? d (first %)) shifted-curve))))
-  )
+(defn pick-rate-by-date [ ^LocalDate d index-curve]
+  (let [shifted-curve (for [ [ d r ] index-curve] [ (jt/plus d (jt/days -1)) r])]
+    (second (last (filter #(jt/after? d (first %)) shifted-curve)))))
+  
 
-(defn apply-curve [ curves float-info ]
+(defn apply-curve [ curves float-info]
   (let [current-curve ((:index float-info) curves)
         reset-dates (:reset float-info)
         index-rate-at-dates (loop [ d reset-dates r []]
-                        (if (nil? d)
-                          r
-                          (recur (next d)
-                                 (conj r (pick-rate-by-date (first d) current-curve))
-                                 )))
-        ]
+                             (if (nil? d)
+                               r
+                               (recur (next d)
+                                      (conj r (pick-rate-by-date (first d) current-curve)))))]
+                                 
+        
     (m/match float-info
-      { :margin mg }
-        (map #(+ mg %) index-rate-at-dates)
-      { :factor ft }
-        (map #(* ft %) index-rate-at-dates)
-      :else nil
-      )
-  ))
+      { :margin mg}
+      (map #(+ mg %) index-rate-at-dates)
+      { :factor ft}
+      (map #(* ft %) index-rate-at-dates)
+      :else nil)))
+      
+  
 
 
-(defn setup-curve [ index dates rates ]
-  (let [ pairs (map vector dates rates )
+(defn setup-curve [ index dates rates]
+  (let [ pairs (map vector dates rates)
         p (sort-by first jt/before? pairs)]
-    { index p}) )
+    { index p}))
 
-(defn curve-to-df [ n ps ]
+(defn curve-to-df [ n ps]
   (let [ dates (into-array LocalDate (map first ps))
         rs (double-array (map second ps))]
     (DoubleFlow. (name n) dates rs)))
 
-(defn smm2cpr [ ^Double smm ]
+(defn smm2cpr [ ^Double smm]
   (- 1 (Math/pow (- 1 smm) 12)))
 
-(defn cpr2smm [ ^Double cpr ]
+(defn cpr2smm [ ^Double cpr]
   (- 1 (Math/pow (- 1 cpr) 1/12)))
 
-(defn cdr2smm [ ^Double cdr ]
+(defn cdr2smm [ ^Double cdr]
   (cpr2smm cdr))
 
-(defn smm2cdr [ ^Double smm ]
+(defn smm2cdr [ ^Double smm]
   (smm2cpr smm))
-
 
 (defn gen-pool-assump-df [curve-type v observe-dates]
   (let [d-intervals (u/gen-dates-interval observe-dates)
         ;days-intervals (map #(jt/time-between (first %) (second %) :days) d-intervals)
-        interval-start (into-array LocalDate (map first d-intervals))
-        interval-end (into-array LocalDate (map second d-intervals)) ]
+        interval-start (u/dates (map first d-intervals))
+        interval-end (u/dates (map second d-intervals))]
     (as->
       (m/match [curve-type v]
                ;[:smm (v :guard #(vector? %))]
                ;(map #(- 1 (Math/pow (- 1 (second %)) (first %))) factors-m)
                [(:or :cpr :cdr) (v :guard #(vector? %))]
-               (let [ daily-pct (map #(- 1 (Math/pow (- 1 %) (/ 1 365))) v ) ]
-                 daily-pct )
+               (let [ daily-pct (map #(- 1 (Math/pow (- 1 %) (/ 1 365))) v)]
+                 daily-pct)
                :else nil) rs
-      (RateAssumption. (name curve-type) interval-start interval-end (double-array rs ) )
-    )
-  ))
+      (RateAssumption. (name curve-type) interval-start interval-end (double-array rs)))))
+    
+  
 
 (defn gen-asset-assump
   [^RateAssumption pool-assumption observe-dates]
   (let [obs-ary (into-array LocalDate observe-dates)]
-    (.apply pool-assumption obs-ary))
-  )
+    (if (= (alength obs-ary) 0)
+      nil
+      (.apply pool-assumption obs-ary))
+    ))
+  
