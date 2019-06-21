f5c65d78b985da2c0fdd0c819f5165548de510d3
xyzhang <always.zhang@gmail.com>
2019-06-21 22:48:29 +0800

    extending a new text representation 'yyyy-mm-dd,yyyy-mm-ddQ,yyyy-mm-dd'

--- src/clj/clojucture/reader/base.clj
+++ src/clj/clojucture/reader/base.clj
@@ -10,26 +10,40 @@
   )
 
 (defn parsing-dates [x]
-
     (m/match x
-             #"\d{4}\-\d{2}\-\d{2}Q,\d{4}\-\d{2}\-\d{2}"
-             (as-> (re-matches #"(\S+)Q,(\S+)" x ) [ _ s ed]
-                   (concat
-                     (u/gen-dates-range (jt/local-date s) (jt/months 3) (jt/local-date ed))
-                     [(jt/local-date ed)])
+      #"^\d{4}\-\d{2}\-\d{2}Q,\d{4}\-\d{2}\-\d{2}$"
+        (as-> (re-matches #"^(\d{4}\-\d{2}\-\d{2})Q,(\d{4}\-\d{2}\-\d{2})$" x ) [ _ s ed]
+          (concat
+            (u/gen-dates-range (jt/local-date s) (jt/months 3) (jt/local-date ed))
+                     [(jt/local-date ed)]))
+
+       #"^\d{4}\-\d{2}\-\d{2},\d{4}\-\d{2}\-\d{2}$"
+             (as-> (re-matches #"(\d{4}\-\d{2}\-\d{2}),(\d{4}\-\d{2}\-\d{2})" x) [_ f l]
+                   ;(println "FL" f l)
+                   (list (parsing-dates f) (parsing-dates l))
                    )
 
-             #"(\S+),(\S+)"
-             (as-> (re-matches #"(\S+),(\S+)" x) [_ first-match last-match]
+      #"(\S+?),(\S+)"
+       (as-> (re-matches #"(\S+?),(\S+)" x) [_ first-match last-match]
+           (flatten (list (parsing-dates first-match) (parsing-dates last-match)  )))
+
+
+      #"(\S+),(\S+),?"
+        (as-> (re-matches #"(\S+),(\S+)" x) [_ first-match last-match]
                    (flatten (list (parsing-dates first-match) (parsing-dates last-match))))
-             #"\d{4}\-\d{2}\-\d{2}"
-             (jt/local-date x)
-             #"\d{4}\-\d{2}\-\d{2}M,\d{4}\-\d{2}\-\d{2}" ;first date as s, following month at day d
+
+
+
+      #"\d{4}\-\d{2}\-\d{2}"
+                 (jt/local-date x)
+
+       #"\d{4}\-\d{2}\-\d{2}M,\d{4}\-\d{2}\-\d{2}" ;first date as s, following month at day d
              (as-> (re-matches #"(\S+)M,(\S+)"  x) [ _ s ed]
                    (concat
                      (u/gen-dates-range (jt/local-date s) (jt/months 1) (jt/local-date ed))
                      [(jt/local-date ed)]))
-           #"\d{4}\-\d{2}\-\d{2}ME,\d{4}-\d{2}\-\d{2}"
+
+       #"\d{4}\-\d{2}\-\d{2}ME,\d{4}-\d{2}\-\d{2}"
            (as-> (re-matches #"(\S+)ME,(\S+)" ,x ) [ _ s ed]
                          (cons (jt/local-date s)
                        (u/gen-period-end-dates
@@ -37,7 +51,7 @@
                          (jt/months 1)
                          (jt/local-date ed))))
 
-           #"\d{4}\-\d{2}\-\d{2}\w{1,2}[+-]\dWD,\d{4}\-\d{2}-\d{2}"
+       #"\d{4}\-\d{2}\-\d{2}\w{1,2}[+-]\dWD,\d{4}\-\d{2}-\d{2}"
               (as-> (re-matches #"(\d{4}\d)WD,(\S+)" x) [ _ s adj days ed ]
                  (let [ bech-dates (parsing-dates s  )
                        adj-days (Integer/parseInt days)
@@ -47,5 +61,5 @@
                                    ) ]
                    adj-dates))
 
-           :else nil )
+           :else (println "failed with " x ) )
   )
--- test/clojucture/reader/base_test.clj
+++ test/clojucture/reader/base_test.clj
@@ -6,19 +6,27 @@
 
 
 (deftest t-parsing-dates
-  (let [ a (base/parsing-dates "2018-01-23")]
-    (is (= a (jt/local-date 2018 1 23))))
+  (comment
+    (let [a (base/parsing-dates "2018-01-23")]
+      (is (= a (jt/local-date 2018 1 23))))
 
 
-  (let [ [ a b ]  (base/parsing-dates "2019-01-01,2011-01-01")]
-    (is (= (jt/local-date 2019 1 1) a))
-    (is (= (jt/local-date 2011 1 1) b)) )
+    (let [[a b] (base/parsing-dates "2019-01-01,2011-01-01")]
+      (is (= (jt/local-date 2019 1 1) a))
+      (is (= (jt/local-date 2011 1 1) b)))
 
-  (let [ x (base/parsing-dates "2013-03-20Q,2014-06-30")]
+    (let [x (base/parsing-dates "2013-03-20Q,2014-06-30")]
 
-    (is (= (first x) (jt/local-date 2013 3 20)) )
-    (is (= (second x) (jt/local-date 2013 6 20)))
+      (is (= (first x) (jt/local-date 2013 3 20)))
+      (is (= (second x) (jt/local-date 2013 6 20)))
+      (is (= (last x) (jt/local-date 2014 6 30)))
+      )
+    )
+  (let [ x (base/parsing-dates "2013-02-01,2013-03-20Q,2014-06-30")]
+    (println x)
+    (is (= (first x)  (jt/local-date 2013 2 1)))
+    (is (= (second x)  (jt/local-date 2013 3 20)))
+    (is (= (nth x 2)  (jt/local-date 2013 6 20)))
     (is (= (last x) (jt/local-date 2014 6 30)))
     )
-
   )
