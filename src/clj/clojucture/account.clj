f888f31424c884c4bd9002f4d0974b2268986d42
xyzhang <always.zhang@gmail.com>
2019-07-20 17:14:06 +0800

    Enhance: add a new arity for `transfer-fund` to transfer all available cash to target account

--- src/clj/clojucture/account.clj
+++ src/clj/clojucture/account.clj
@@ -8,35 +8,39 @@
   )
 
 
-(defrecord stmt [ ^LocalDate date from to ^Double amount info ] )
+(defrecord stmt [^LocalDate date from to ^Double amount info])
 
 
-(defrecord account [ name type ^Double balance stmts ]
+(defrecord account [name type ^Double balance stmts]
   t/Account
-  (withdraw [ x d to amount ]
+  (withdraw [x d to amount]
     (.deposit x d to (- amount))
     )
-  (try-withdraw [ x d to amount ]
-    (let [ max-to-draw (min amount balance)]
-    (.deposit x d to (- max-to-draw)))
+  (try-withdraw [x d to amount]
+    (let [max-to-draw (min amount balance)]
+      (.deposit x d to (- max-to-draw)))
     )
 
-  (deposit [ x  d from amount ]
-    (let [ new-statment (->stmt d from :this amount nil)
-           new-balance (+ balance amount)]
+  (deposit [x d from amount]
+    (let [new-statment (->stmt d from :this amount nil)
+          new-balance (+ balance amount)]
       (->account name type new-balance (conj stmts new-statment))
       )
     )
 
-  (last-txn [ x ]
+  (last-txn [x]
     (last stmts)
     )
   )
 
-(defn transfer-fund [ from-acc to-acc ^LocalDate d ^Double amount ]
-  (if (>= (:balance from-acc) amount)
-    (dosync
-      [ (.withdraw from-acc d to-acc amount) (.deposit to-acc d from-acc amount)]
-      )
-    nil)
-  )
+(defn transfer-fund
+  ([from-acc to-acc ^LocalDate d ]
+   (let [ transfer-amt (:balance from-acc)]
+     (transfer-fund from-acc to-acc d transfer-amt) ))
+  ([from-acc to-acc ^LocalDate d ^Double amount]
+   (if (>= (:balance from-acc) amount)
+     (dosync
+       [(.withdraw from-acc d to-acc amount) (.deposit to-acc d from-acc amount)]
+       )
+     :not-enought-cash)))
+
