6aad750d2cf11b547b7c27116136b1e7be161a62
xyzhang <always.zhang@gmail.com>
2019-07-31 00:02:07 +0800

    bug fix: correct sample-cf in cashflow_test.clj

--- test/clojucture/cashflow_test.clj
+++ test/clojucture/cashflow_test.clj
@@ -2,19 +2,33 @@
   (:require [clojure.test :refer :all]
             [clojucture.asset :as asset]
             [java-time :as jt]
-            [clojucture.util :as u]))
-            
+            [clojucture.util :as u])
+  (:import
+    [tech.tablesaw.api DoubleColumn DateColumn]
+    [tech.tablesaw.columns AbstractColumn]
+    [clojucture Cashflow]
+    )
+  )
+
 
 (def sample-cf
-  (let [ df (u/gen-dates-ary (jt/local-date 2018 1 1) (jt/months 1) 12)
+  (let [df (u/gen-dates-ary (jt/local-date 2018 1 1) (jt/months 1) 12)
         balf (double-array (range 2200 -1 -200))
-        bf (aset-double (double-array 12 200) 0 0)
-        if (double-array [0 10 20 30 40 50 60 70 80 90 100 110])]
-   (u/gen-cashflow "sample1"
-     [ [:dates df]
-      [:balance balf
-       [:principal bf]
-       [:interest if]]])))
+        bf  (double-array 12 200)
+        if (double-array [0 10 20 30 40 50 60 70 80 90 100 110])
+
+        df-col (DateColumn/create "dates" df)
+        balf-col (DoubleColumn/create "balance" balf)
+        bf-col (DoubleColumn/create "principal" bf)
+        if-col (DoubleColumn/create "interest" if)
+
+        column-array (into-array AbstractColumn [df-col balf-col bf-col if-col])
+        ]
+    (doto (Cashflow. "sample1")
+      (.addColumns column-array ) )
+    )
+
+  )
       
 
 
