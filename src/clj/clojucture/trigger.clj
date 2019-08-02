fb457bb988f9a699399f2f6b7128ec5b8ae8f51a
xyzhang <always.zhang@gmail.com>
2019-08-02 23:54:24 +0800

    Add: triggers with a threshold vector

--- src/clj/clojucture/trigger.clj
+++ src/clj/clojucture/trigger.clj
@@ -1,7 +1,8 @@
 (ns clojucture.trigger
   (:require [clojure.core.match :as m]
-            [clojucture.util-cashflow :as cfu])
-  )
+            [clojucture.util-cashflow :as cfu]
+            [clojucture.util :as u])
+  (:import (clojucture RateAssumption)))
 
 
 
@@ -23,13 +24,38 @@
 (defn run-pool-trigger [trigger pool-cf]
   (let [p (:p trigger)]
     (m/match p
-             {:target :pool-cumulative-default-rate :op op :threshold threshold}
+             {:target :pool-cumulative-default-rate :op op :threshold threshold :curable true}
              (let [ trigger-flow (.select pool-cf (into-array String ["dates" "default[cumSum]"]))
-                   test-level-flow (-> (.column trigger-flow 1) (.asList))
-                   test-result (map #(op % threshold) test-level-flow)
-                   ]
-                 test-result
+                   test-level-flow (-> (.column trigger-flow 1) (.asList)) ]
+                 (map #(op % threshold) test-level-flow)
                )
+             {:target :pool-cumulative-default-rate :op op :threshold-vec threshold-v :curable true}
+             (let [trigger-flow (.select pool-cf (into-array String ["dates" "default[cumSum]"]))
+                   ;start-date (.get d-column 0)
+                   ;end-date (.get d-column (dec n-rows))
+                   trigger-vec-df (RateAssumption. "vTrigger" (u/dates (first threshold-v)) (u/ldoubles (second threshold-v)))
+                   ]
+               (loop [ cr (.next (.iterator trigger-flow)) r [] ]
+                 (if (.hasNext cr)
+                   (let [ cd (.getDate cr "dates")
+                         cv (.getDouble cr "default[cumSum]")
+                         test-value (.rateAt trigger-vec-df cd)
+                         ;_ (println cd cv test-value)
+                         ]
+                     (recur (.next cr) (conj r (op cv test-value))))
+                   r )))
+
+             {:target :pool-cumulative-default-rate :op op :threshold-vec threshold-v }
+             (let [curable-p (assoc p :curable true)
+                   curable-t (assoc trigger :p curable-p)
+                   curable-v (run-pool-trigger curable-t pool-cf)
+                   v-len (count curable-v)
+                   unbreached (take-while false? curable-v) ]
+               (concat unbreached (repeat (- v-len (count unbreached)) true))
+               )
+             {:target :pool-cumulative-default-rate :op op :threshold threshold }
+             nil
+
              :else :not-match-trigger
              )
     )
--- test/clojucture/trigger_test.clj
+++ test/clojucture/trigger_test.clj
@@ -3,7 +3,9 @@
             [clojucture.util :as u]
             [clojucture.trigger :as trg]
             [java-time :as jt]
-            [clojucture.pool :as p]))
+            [clojucture.pool :as p])
+  (:import (clojucture DoubleFlow))
+  )
 
 
 
@@ -11,20 +13,51 @@
   (u/gen-cashflow "pool-cf-test"
                      [[:dates (u/gen-dates-ary (jt/local-date 2018 1 1) (jt/months 2) 10)]
                       [:default (double-array 10 0.01)]
-                      ] )
+                      ]))
 
-  )
+(def test-pool-cf2
+  (u/gen-cashflow "pool-cf-test"
+                     [[:dates (u/gen-dates-ary (jt/local-date 2018 1 1) (jt/months 2) 18)]
+                      [:default (double-array 18 0.001)]
+                      ]))
 
 (def test-pool-cf-cum (p/calc-cumulative-amount test-pool-cf "default"))
+(def test-pool-cf-cum2 (p/calc-cumulative-amount test-pool-cf2 "default"))
 
 (deftest tPoolTrigger
-  (let [ cum-trigger (trg/->pool-trigger "pool-t" {:target :pool-cumulative-default-rate :op > :threshold 0.03 } false)
-         t-result (trg/run-pool-trigger cum-trigger test-pool-cf-cum)
+  (let [ cum-trigger (trg/->pool-trigger "pool-t" {:target :pool-cumulative-default-rate :op > :threshold 0.03 :curable true} false)
+        t-result (trg/run-pool-trigger cum-trigger test-pool-cf-cum)
+
+        threshold-vector [ [ (jt/local-date 2018 1 1) (jt/local-date 2019 1 1) (jt/local-date 2020 12 1 ) ] [ 0.005 0.01 ]]
+        cum-trigger-vec (trg/->pool-trigger "pool-t"
+                                            {:target :pool-cumulative-default-rate :op >
+                                             :threshold-vec threshold-vector :curable true } false)
+        t-result-vec (trg/run-pool-trigger cum-trigger-vec test-pool-cf-cum2)
+
+        cum-trigger-vec2 (trg/->pool-trigger "pool-uncurable"
+                                            {:target :pool-cumulative-default-rate :op >
+                                             :threshold-vec threshold-vector :curable false } false)
+
+        t-result-vec2 (trg/run-pool-trigger cum-trigger-vec2 test-pool-cf-cum2)
         ]
+    ;single value test
     (is (= (nth t-result 2) false))
     (is (= (nth t-result 0) false))
     (is (= (nth t-result 8) true))
     (is (= (nth t-result 9) true))
+
+
+    ;vector values test
+    ;(println t-result-vec)
+    (is (= (nth t-result-vec 5) true))
+    (is (= (nth t-result-vec 10) true))
+    (is (= (nth t-result-vec 16) true))
+    ;(is (= (nth t-result-vec 17) true))
+
+    (is (= (nth t-result-vec2 5) true))
+    (is (= (nth t-result-vec2 6) true))
+    (is (= (nth t-result-vec2 16) true))
+
     )
   )
 
