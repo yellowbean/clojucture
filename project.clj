2f35e1bc09b4037f88e76734560325ba4abd8ccd
xyzhang <always.zhang@gmail.com>
2019-08-14 22:50:53 +0800

    Add server mode

--- project.clj
+++ project.clj
@@ -2,39 +2,44 @@
   :description "a clojure library for modelling & analysis structure products (CLO/MBS/ABS)"
   :url "https://yellowbean.github.io/clojucture/"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-    [org.clojure/clojure "1.10.1"]
-    [tech.tablesaw/tablesaw-core "0.32.7"]
-    [org.apache.commons/commons-math3 "3.6.1"]
-    [org.apache.commons/commons-lang3 "3.7"]
-    [org.threeten/threeten-extra "1.4"]
-    [nrepl "0.6.0"]
-    [clojure.java-time "0.3.2"]
-    [org.clojure/data.csv "0.1.4"]
-    [org.clojure/data.json "0.2.6"]
-    [org.clojure/data.zip "0.1.2"]
-    [org.clojure/core.match "0.3.0"]
-    [dk.ative/docjure "1.12.0"]
-    [medley "1.2.0"]
-   ]
+                 [org.clojure/clojure "1.10.1"]
+                 [tech.tablesaw/tablesaw-core "0.32.7"]
+                 [org.apache.commons/commons-math3 "3.6.1"]
+                 [org.apache.commons/commons-lang3 "3.7"]
+                 [org.threeten/threeten-extra "1.4"]
+                 [nrepl "0.6.0"]
+                 [clojure.java-time "0.3.2"]
+                 [org.clojure/data.csv "0.1.4"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/data.zip "0.1.2"]
+                 [org.clojure/core.match "0.3.0"]
+                 [dk.ative/docjure "1.12.0"]
+                 [org.clojure/tools.cli "0.4.2"]
+                 [medley "1.2.0"]
+
+                 [ring "1.7.1"]
+                 [compojure "1.6.1"]
+                 ]
   :plugins [
             [lein-virgil "0.1.9"]
             ]
   :main clojucture.core
   :mirrors {"central" {:name "aliyun maven"
-                       :url "https://maven.aliyun.com/nexus/content/groups/public/"}}
+                       :url  "https://maven.aliyun.com/nexus/content/groups/public/"}}
   ;:user {:plugins [ [nightlight/lein-nightlight "RELEASE"]]}
   :source-paths ["src/clj"]
-  :java-source-paths [ "src/java" ]
+  :java-source-paths ["src/java"]
   :profiles {
-   :uberjar {:aot :all}
-   :dev {
-    :plugins [
-      [ lein-shell "0.5.0"]
-    ]
-   }
-  }
+             :uberjar {:aot :all}
+             :dev     {
+                       :plugins [
+                                 [lein-ring "0.12.5"]
+                                 [lein-shell "0.5.0"]
+                                 ]
+                       }
+             }
   :aliases
   {"native"
    ["shell"
@@ -42,4 +47,5 @@
     "--initialize-at-build-time"
     "-jar" "./target/${:uberjar-name:-${:name}-${:version}-standalone.jar}"
     "-H:Name=./target/${:name}"]}
-)
+  :ring {:handler clojucture.server/shandler}
+  )
--- src/clj/clojucture/core.clj
+++ src/clj/clojucture/core.clj
@@ -1,11 +1,35 @@
 (ns clojucture.core
+  (:require
+    [clojure.core.match :as m]
+    [clojure.java.io :as io]
+    [clojure.tools.cli :refer [parse-opts]]
+    )
   (:import
     [java.time LocalDate])
   (:gen-class)
   )
 
+(def engine-version 0.1)
+
+
+(def cli-options
+  [
+   ["-p" "--port PORT" "Port number"
+      :default 15013
+      :parse-fn #(Integer/parseInt %)
+      :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
+
+   ["-c" "--config CONFIG" "Config file path"]
+      :default nil
+      :validate [ #(.exists (io/as-file %))  "Config file doesn't exists"]
+
+   ["-h" "--help"]
+  ])
+
 
 
 (defn -main [ & args]
-  (println "reading from input"))
+  (let [ cli-m (parse-opts args cli-options)]
+
+  ))
 
