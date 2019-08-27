61ac6e0f81a6ec4e814314a0a9f206a495f84622
xyzhang <always.zhang@gmail.com>
2019-08-28 00:48:12 +0800

    add `flavor` for reading different country deal types with different reader

--- src/clj/clojucture/server.clj
+++ src/clj/clojucture/server.clj
@@ -3,6 +3,7 @@
     [clojure.data.json :as json]
     [clojure.java.io :as io]
     [clojure.tools.cli :as cli]
+    [clojure.core.match :as m]
 
     [compojure.core :refer :all]
     [compojure.route :as route]
@@ -19,38 +20,43 @@
   )
 
 (def version 0.03)
+;(def config (atom {:port 3001 :directory "C:\\changer\\engine\\resources"}))
 (def config (atom {}))
 
 
-(defn list-deals [ ws-id ]
-  (let [
-        f-list (->>
-                 (seq (.list (io/file (:directory @config) ws-id)))
-                 (filter #(.endsWith % ".edn")))
-        ]
-    {:workspace ws-id :models f-list}
+(defn list-deals [ws-id]
+  "list all deals(files end with `edn`) in workspace"
+  (let [all-file-list (seq (.list (io/file (:directory @config) ws-id))) ]
+    {:workspace ws-id :models (filter #(.endsWith % ".edn") all-file-list )}
     ))
 
-(defn read-deal [ws-id deal-file-name]
+(defn read-deal [ws-id deal-file-name flavor]
+  "read deal file and return in a map"
   (let [deal-file-path (io/file (:directory @config) ws-id deal-file-name)]
-    (cn/load-deal-from-file (.toString deal-file-path))))
+    (case flavor
+      "china" (cn/load-deal-from-file (.toString deal-file-path))
 
-(defn run-deal [ws-id deal-file-name assump]
-  (let [deal-loaded (read-deal ws-id deal-file-name)]
-    (:projection (cn/run-deal deal-loaded assump))))
 
+      {:error (str "No flavor found for " flavor)}
+      )
+    ))
+
+(comment
+  (defn run-deal [ws-id deal-file-name assump]
+    (let [deal-loaded (read-deal ws-id deal-file-name)]
+      (:projection (cn/run-deal deal-loaded assump))))
+  )
 
 (defroutes local-server
            (GET "/" [] "<h3>Alive!</h3>")
            (GET "/version" [] (-> {:version version} (json/write-str)))
            (GET "/ping" [] (-> {:version version :ip "localhost"} (json/write-str)))
+           (GET "/workspaces" [] (-> {:workspaces ["A" "B"]} (json/write-str)))
            (context "/workspace/:ws-id" [ws-id]
              (GET "/" [] (-> {:workspace ws-id} (json/write-str)))
              (GET "/deals" [] (-> (list-deals ws-id) (json/write-str)))
-             (context "/:d-name" [d-name]
-               (GET "/" [] (-> (read-deal ws-id d-name) (json/write-str)))
+             (GET "/deal/:d-name" [ d-name flavor] (-> (read-deal ws-id d-name flavor) (json/write-str)))
                ;(GET "/run" [ assump ] (-> (read-deal ws-id d-name ) (cn/run-deal assump) (json/write-str)))
-               )
              )
            )
 
@@ -78,11 +84,10 @@
 
    ["-h" "--help"]
    ]
-   )
+  )
 
 (defn -main [& args]
-  (let [cli-m (cli/parse-opts args cli-options) ]
+  (let [cli-m (cli/parse-opts args cli-options)]
     (reset! config (:options cli-m))
-    (println @config)
     (start-server @config)
     ))
