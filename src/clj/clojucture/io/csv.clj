ecf99cefd0d4c083bae16e178cf673c4870d4413
xyzhang <always.zhang@gmail.com>
2019-11-24 17:15:44 +0800

    add a cashflow reader function in ns:csv, taking in csv file as an input

--- src/clj/clojucture/io/csv.clj
+++ src/clj/clojucture/io/csv.clj
@@ -1,26 +1,34 @@
 (ns clojucture.io.csv
   (:require [clojure.java.io :as io])
-  (:import [tech.tablesaw.api Table ColumnType ]
-           [tech.tablesaw.io.csv CsvWriter CsvWriteOptions CsvReader ] 
-           [tech.tablesaw.io Source] 
-           
-           ) )
+  (:import [tech.tablesaw.api Table ColumnType]
+           [tech.tablesaw.io.csv CsvWriter CsvReader CsvReadOptions$Builder CsvReadOptions]
+           [tech.tablesaw.io Source]
+           (java.util Locale)))
 
 
-(defn column-type-mapping [x]
- (case x
-  :double ColumnType/DOUBLE 
-  :boolean ColumnType/BOOLEAN 
-  :date ColumnType/LOCAL_DATE 
-  :int ColumnType/INTEGER
-  :string ColumnType/STRING
-  ))
+(defn- column-type-mapping [x]
+  (case x
+    :double ColumnType/DOUBLE
+    :boolean ColumnType/BOOLEAN
+    :date ColumnType/LOCAL_DATE
+    :int ColumnType/INTEGER
+    :string ColumnType/STRING
+    ))
 
 
 
-(defn read-cf [ csv-name field-types ]
+(defn read-cf [csv-name field-types]
   "internal function to read csv files into Cashflow"
-    (let [s (Source. (io/as-file  (io/resource csv-name)) )
-          field-types (mapv #(column-type-mapping %) field-types) 
-        csv-reader (CsvReader. field-types ) ]
-      (.read csv-reader s ))) 
+  (let [ f (io/as-file (io/resource csv-name))
+        reader-opts (doto
+                      (CsvReadOptions/builder f)
+                      (.header true)
+                      (.locale Locale/US)
+                      (.missingValueIndicator "")
+                      (.separator (char 44)) ;44 => , in ANSI
+                      )
+        field-types (mapv #(column-type-mapping %) field-types)
+        csv-reader (CsvReader. field-types)
+        ]
+    (.read csv-reader (.build reader-opts) )
+    ))
